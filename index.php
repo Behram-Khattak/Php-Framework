d43cf7a7fae0994e5f053e224621519bfb179945
Behram-Khattak <behramkttk9@gmail.com>
2023-12-25 20:38:34 +0500

    Workingon index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
-
+/**
+ * This try catch block is used to run the main,
+ * view file of the framework if the file exist's.
+ */
 try {
-    if (! require_once "./views/master.php")
-        throw new Exception("File not found !");
+    if (! include_once __DIR__ . '/views/master.php')
+     {
+        throw new Exception(404 .' '. "Page not found !");
+     }
 
 } catch(Exception $e) {
     echo $e->getMessage();
