3492f072d84f348e861442a59e9850e55531a174
Behram-Khattak <behramkttk9@gmail.com>
2024-06-04 12:43:52 +0500

    Updated index.php file

--- public/index.php
+++ public/index.php
@@ -4,10 +4,9 @@
  * view file of the framework if the file exist's.
  */
 try {
-    if (! include_once __DIR__ . '../')
-     {
-        throw new Exception(404 .' '. "Page not found !");
-     }
+    
+    include_once __DIR__. '/../views/welcome.php' or
+    throw new Exception(404, "Page not found !");
 
 } catch(Exception $e) {
     echo $e->getMessage();
---
+++ views/welcome.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Welcome to the php framework for small web applications !";
